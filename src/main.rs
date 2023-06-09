85a20fe4ec40a79d2dac2c1ec5c4952f668cc2e0
gael <76548719+gaelzarco@users.noreply.github.com>
2023-06-09 10:41:41 -0700

    constants and let

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+// VARIBALES AND MUTABILITY
+
 fn main() {
-    println!("Hello, world!");
+    // vars are immutable by default
+    let x: i32 = 5;
+    println!("The value of x is: {x}");
+   //  x = 6; error: cannot assign twice to immutable variable `x`
+
+    let mut y: i32 = 10;
+    println!("The value of y is: {y}");
+    y = 11;
+    println!("The value of y is: {}", y);
+
+    // constants
+    // cannot use mut with constants, must annotate type, name is uppercase, use underscores
+    const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+    println!("Three hours in seconds is: {}", THREE_HOURS_IN_SECONDS);
+
 }
